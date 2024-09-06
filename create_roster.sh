7551d0b06f9152333eed7a94bab1dafc5d55dab1
student <remington.neustadter@atlasschool.com>
2024-09-06 07:31:39 +0000

    hold spot

--- create_roster.sh
+++ create_roster.sh
@@ -4,24 +4,25 @@
 names=()
 ages=()
 
-number_of_students=0
+number_of_students  
 #Intro
 
 echo "Welcome to the Interactive student Roster Creator!"
-echo "How many students do you have to add?" 
-read -p number_of_students
+echo "How many students do you have to add? " 
+read -p $number_of_students
 
 # Access elements
 #Name
-for (i in $number_of_students; i++); do
-    echo "Enter Student Name" ; read -p $((i+1)): ${names[i]}"
+ i=$number_of_students;for  i in "${!names[@]}"; do
+	read -p "Enter student $((i+1)) name: " sth
+       names[$i]=$sth       
 done
-
-
-# Loop through arrays
-for ((i=0; i<${#names[@]}; i++)); do
-    echo "Student $((i+1)): ${names[i]}"
-done
+echo "${names[@]}"
+echo "$number_of_students"
+###### Loop through arrays
+####for ((i=0; i<${#names[@]}; i++)); do
+ ####   echo "Student $((i+1)): ${names[i]}"
+####done
 
 
 
