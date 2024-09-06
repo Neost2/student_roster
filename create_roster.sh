c2b59851872cfe368ce866344ac0cc7f64c761eb
Neost2 <Neost2@hotmail.com>
2024-09-05 23:15:15 -0500

    another test

--- create_roster.sh
+++ create_roster.sh
@@ -3,28 +3,24 @@
 # Declare arrays
 names=()
 ages=()
-number_of_students=()
 
-
-#dDelcare elements
-nm
-ag
-nb
+number_of_students=0
+#Intro
 
 echo "Welcome to the Interactive student Roster Creator!"
 echo "How many students do you have to add?" 
-read -p ${number_of_students[nb]}
+read -p number_of_students
 
 # Access elements
-echo "Enter students name:"
-read -p ${names[nm]}
-echo "Enter students age:" 
-read -p ${age[ag]}
+#Name
+for ((i in ${number_of_students}; i++)); do
+    echo "Enter Student Name" ; read -p $((i+1)): ${names[i]}"
+done
 
 
 # Loop through arrays
 for ((i=0; i<${#names[@]}; i++)); do
-    echo "Student $((i+1)): ${names[i]}, Age: ${ages[i]}"
+    echo "Student $((i+1)): ${names[i]}"
 done
 
 
