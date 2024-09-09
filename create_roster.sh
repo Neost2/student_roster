f111860184bac15124ebf1b0b808779b57aff04d
Neost2 <Neost2@hotmail.com>
2024-09-09 17:46:15 -0500

    Modified the loop for the arrays to work think I was over thinking it. This should now put names into the arrays.

--- create_roster.sh
+++ create_roster.sh
@@ -4,23 +4,24 @@
 names=()
 ages=()
 
-number_of_students=0
 #Intro
 
 echo "Welcome to the Interactive student Roster Creator!"
-echo "How many students do you have to add?" 
-read -p number_of_students
+read -p "How many students do you have to add?" number_of_students
+
 
 # Access elements
 #Name
-for (i in $number_of_students; i++); do
-    echo "Enter Student Name" ; read -p $((i+1)): ${names[i]}"
+for ((i=0; i<${#number_of_students[@]}; i++)); do
+    read -p "Please enter in students name: " names(i)
+    read -p "Please enter in students age: " age(i)
 done
 
 
 # Loop through arrays
 for ((i=0; i<${#names[@]}; i++)); do
-    echo "Student $((i+1)): ${names[i]}"
+    echo "Student name is  $((i+1)): ${names[i]}"
+    echo "Student age is $((i+1)): ${ages[i]}"
 done
 
 
