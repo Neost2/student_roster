41dfef404e095932022d58ca5843acf78c6c8a45
root <root@Comp>
2024-09-09 19:05:26 -0500

    Got it working figured out what I was doing wrong with the array for loop. To assign the elements.

--- create_roster.sh
+++ create_roster.sh
@@ -4,7 +4,7 @@
 names=()
 ages=()
 
-<<<<<<< HEAD
+##<<<<<<< HEAD
 #Intro
 
 echo "Welcome to the Interactive student Roster Creator!"
@@ -13,38 +13,21 @@ read -p "How many students do you have to add?" number_of_students
 
 # Access elements
 #Name
-for ((i=0; i<${#number_of_students[@]}; i++)); do
-    read -p "Please enter in students name: " names(i)
-    read -p "Please enter in students age: " age(i)
+
+for  (( i=0; i<${number_of_students}; i++)); do
+
+    read -p "Please enter in students name: " nam
+	names[i]=$nam
+    read -p "Please enter in students age: " age
+	ages[i]=$age
+
 done
 
 
 # Loop through arrays
 for ((i=0; i<${#names[@]}; i++)); do
-    echo "Student name is  $((i+1)): ${names[i]}"
-    echo "Student age is $((i+1)): ${ages[i]}"
-=======
-number_of_students  
-#Intro
-
-echo "Welcome to the Interactive student Roster Creator!"
-echo "How many students do you have to add? " 
-read -p $number_of_students
-
-# Access elements
-#Name
- i=$number_of_students;for  i in "${!names[@]}"; do
-	read -p "Enter student $((i+1)) name: " sth
-       names[$i]=$sth       
->>>>>>> 7551d0b06f9152333eed7a94bab1dafc5d55dab1
+    echo "Student $((i+1)): ${names[i]}"
+    echo "Age is $((i+1)): ${ages[i]}"
 done
-echo "${names[@]}"
-echo "$number_of_students"
-###### Loop through arrays
-####for ((i=0; i<${#names[@]}; i++)); do
- ####   echo "Student $((i+1)): ${names[i]}"
-####done
-
-
 
 
